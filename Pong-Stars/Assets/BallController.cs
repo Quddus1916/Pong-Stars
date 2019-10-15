2ce5d18c86864ad9a815bc0728692d47402ac2c0
Quddus1916 <56597743+Quddus1916@users.noreply.github.com>
2019-10-15 21:37:56 +0600

    Add Project Files

---
+++ Pong-Stars/Assets/BallController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(5f,5f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
